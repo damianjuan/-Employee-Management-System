5892429a145c477ef66c2b71fa4d21ee4adb8282
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-06-09 15:50:52 -0500

    created database and table to hold employees, role, department

--- Assets/schema.sql
+++ Assets/schema.sql
@@ -1,21 +1,26 @@
+DROP DATABASE IF EXISTS employee_db;
 
-* **department**:
+CREATE DATABASE employee_db;
 
-  * **id** - INT PRIMARY KEY
-  * **name** - VARCHAR(30) to hold department name
+USE employee_db;
 
-* **role**:
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
+);
 
-  * **id** - INT PRIMARY KEY
-  * **title** -  VARCHAR(30) to hold role title
-  * **salary** -  DECIMAL to hold role salary
-  * **department_id** -  INT to hold reference to department role belongs to
+CREATE TABLE role (
+    id INT PRIMARY KEY
+    title VARCHAR(30) 
+    salary DECIMAL
+    department_id INT
+);
 
-* **employee**:
+CREATE TABLE employee (
+    id INT PRIMARY KEY
+    first_name VARCHAR(30)
+    last_name VARCHAR(30)
+    role_id INT
+    manager_id INT 
+);
 
-  * **id** - INT PRIMARY KEY
-  * **first_name** - VARCHAR(30) to hold employee first name
-  * **last_name** - VARCHAR(30) to hold employee last name
-  * **role_id** - INT to hold reference to role employee has
-  * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
-  
